9cd1975182f9fbdb8a555d285f509f93c7304a1f
atulgupta05 <atulajitdewantigupta@gmail.com>
2026-01-30 20:39:15 +0530

    SartServer method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,11 @@ app.get("/api/health", (req, res) => {
 //   })
 // }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-  connectDB();
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();

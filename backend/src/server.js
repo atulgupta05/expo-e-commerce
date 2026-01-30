08ba90a2d2942408d44a9720042d2cf8d0106622
atulgupta05 <atulajitdewantigupta@gmail.com>
2026-01-30 20:18:24 +0530

    db config & tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
-import { ENV } from "../config/env.js";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
@@ -8,18 +10,23 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
+app.use(clerkMiddleware());  //req.auth // adds auth object under the req
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({ message: "Success Atul Gupta" });
+  req.auth
+  res.status(200).json({ message: "Success Atul Gupta" });
 })
 
-//make our app for ready for deployment
-if(ENV.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../admin/dist")));
+// //make our app for ready for deployment
+// if(ENV.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin","dist","index.html"));
-  })
-}
+//   app.get("/{*any}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../admin","dist","index.html"));
+//   })
+// }
 
-app.listen(PORT, () => console.log(`Success Atul Gupta , Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+  connectDB();
+});

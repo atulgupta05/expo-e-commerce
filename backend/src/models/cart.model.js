2d9d592e8fda769667faee4bc7963e4f25db5220
atulgupta05 <atulajitdewantigupta@gmail.com>
2026-02-03 22:01:53 +0530

    NEw Files

---
+++ backend/src/models/cart.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { Product } from "./product.model";
+
+
+const cartItemSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+        default: 1
+    }
+})
+
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    clerkId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }, items: [cartItemSchema],
+
+
+}, { timestamps: true })
+
+
+export const Cart = mongoose.model("Cart",cartSchema);
